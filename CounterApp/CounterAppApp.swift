5ff543b40cef7314590c8fca43f5f70b52aa209b
hiro <h.kaneko.baseball@icloud.com>
2023-06-14 12:06:19 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by 金子広樹 on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
